159254bf8c884c656a336c14cdf01e909f0ec3aa
wangvps <99784662+wangvps@users.noreply.github.com>
2022-08-02 02:31:27 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="p01--v2op--lak--j4ul-gklk.code.run";
+        url.hostname="v2op-prod-wang-jbqg45.mo1.mogenius.io";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
